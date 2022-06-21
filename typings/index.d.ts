9c121ecc606e0e5101183d7a90630c6960f01585
ItzDrGato <43690887+drgatoxd@users.noreply.github.com>
2022-06-20 22:56:00 -0500

    fix(typings): export classes and functions
    Exporta las clases y la función Loadders en `typings/index.d.ts`

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -35,3 +35,47 @@ export interface ApplicationCommandStructure {
 export interface ExtendedClientOptions extends Discord.ClientOptions {
   prefix: string;
 }
+
+import Discord from "discord.js";
+import { Client } from "../src/class/Client";
+
+type CommandCategory = "general" | "moderation" | "fun" | "owner";
+
+export interface TextCommandStructure {
+  name: string;
+  description: string;
+  usage: string;
+  aliases?: string[] | [];
+  category: CommandCategory;
+  ownerOnly?: boolean | false;
+  permissions?:
+    | {
+        user: Discord.PermissionString[];
+        bot: Discord.PermissionString[];
+      }
+    | { user: []; bot: [] };
+  run: (client: Client, message: Discord.Message, args: string[]) => void;
+}
+
+export interface ApplicationCommandStructure {
+  name: string;
+  description: string;
+  category: CommandCategory;
+  options?: Discord.ApplicationCommandOption[] | [];
+  ownerOnly?: boolean | false;
+  permissions: {
+    user?: Discord.PermissionString[] | [];
+    bot?: Discord.PermissionString[] | [];
+  };
+  run: (client: Client, interaction: Discord.CommandInteraction) => void;
+}
+
+export interface ExtendedClientOptions extends Discord.ClientOptions {
+  prefix: string;
+}
+
+export * as Client from '../src/class/Client';
+export * as Command from '../src/class/Command';
+export * as Event from '../src/class/Event';
+
+export * as Loadders from '../src/utils/Loadders';
